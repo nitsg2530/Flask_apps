b8d48aa0c9d345149cd21e19fb0d6e8f64095e75
nitsg2530 <nitsg2530@gmail.com>
2021-06-02 10:23:32 +0200

    Update urlshort.py
    Generic save file path

--- url-shortener/urlshort/urlshort.py
+++ url-shortener/urlshort/urlshort.py
@@ -26,7 +26,7 @@ def your_url():
         else:
             f = request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('D:/MyDataProjects/url-shortener/urlshort/static/user_files/' + full_name)
+            f.save('/static/user_files/' + full_name)
             urls[request.form['code']] = {'file': full_name}
 
         with open('urls.json','w') as url_file:
